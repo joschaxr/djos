dacd3f8fec0034c645532477426497de517810a3
joschaxr <finjas5@gmail.com>
2026-07-01 12:28:35 +0200

    Create tracks table migration

--- backend/app/db/test_connection.py
+++ backend/app/db/test_connection.py
@@ -2,7 +2,13 @@ from sqlalchemy import text
 
 from app.db.session import engine
 
-with engine.connect() as connection:
-    result = connection.execute(text("SELECT version();"))
 
-    print(result.scalar())
+with engine.connect() as connection:
+    result = connection.execute(
+        text(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"
+        )
+    )
+
+    for row in result:
+        print(row)
